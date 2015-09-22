7b655d59a95bbfd65e29672e9e2bf7d461096dbb
Serge Rodovnichenko <sergerod@gmail.com>
2015-09-22 18:45:56 +0300

    Minimal working version complete

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,10 +1,9 @@
 <?php
-return array (
-  'name' => 'Automatic SKU ID Generator',
-  'icon' => 'img/asn.gif',
-  'version' => '1.0.0',
-  'vendor' => '--',
-  'handlers' => 
-  array (
-  ),
+return array(
+    'name'     => 'Automatic SKU ID Generator',
+    'icon'     => 'img/asn.gif',
+    'version'  => '1.0.0',
+    'vendor'   => '670917',
+    'handlers' =>
+        array('product_save' => 'hookProductSave'),
 );
