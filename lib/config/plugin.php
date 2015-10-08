daf9f1c956d24312fb6cb6780e40b1d7c9d163f1
Serge Rodovnichenko <sergerod@gmail.com>
2015-10-08 11:55:03 +0300

    Template for SKU name

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -11,7 +11,7 @@
 return array(
     'name'     => 'Генератор имен артикулов',
     'icon'     => 'img/asn.gif',
-    'version'  => '1.0.0',
+    'version'  => '1.1.0',
     'vendor'   => '670917',
     'handlers' =>
         array('product_save' => 'hookProductSave'),
--- lib/shopAsnPlugin.class.php
+++ lib/shopAsnPlugin.class.php
@@ -4,7 +4,7 @@
  * Automatic SKU ID Generator plugin for Shop-Script 6
  *
  * @author Serge Rodovnichenko <serge@syrnik.com>
- * @version 1.0.0
+ * @version 1.1.0
  * @copyright Serge Rodovnichenko, 2015
  * @license http://www.webasyst.com/terms/#eula Webasyst
  * @package asn
@@ -22,11 +22,17 @@ class shopAsnPlugin extends shopPlugin
             if ($product && isset($product['skus']) && is_array($product['skus'])) {
                 $Sku = new shopProductSkusModel();
                 $skus = array();
+                $view = wa()->getView();
+                $template = $this->getSettings('template');
+                if(empty($template)) {
+                    $template = '{$sku_id}';
+                }
+
                 foreach ($product['skus'] as $key => $sku) {
                     $skus[$key] = $sku;
                     if (isset($sku['sku'], $sku['id']) && !strlen($sku['sku'])) {
-
-                        $sku['sku'] = $sku['id'];
+                        $view->assign('sku_id', $sku['id']);
+                        $sku['sku'] = $view->fetch('string:'.$template);
                         $new_sku = $Sku->update($sku['id'], $sku);
                         if ($new_sku) {
                             $skus[$key] = $sku;
