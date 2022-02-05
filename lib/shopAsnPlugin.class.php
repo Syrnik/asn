583bdb2a0321a1a72048ca7b5165b3c07b1c840c
Serge Rodovnichenko <sergerod@gmail.com>
2022-02-06 00:40:36 +0300

    Code reformat

--- lib/shopAsnPlugin.class.php
+++ lib/shopAsnPlugin.class.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * Automatic SKU ID Generator plugin for Shop-Script 6
  *
  * @author Serge Rodovnichenko <serge@syrnik.com>
  * @version 1.1.0
- * @copyright Serge Rodovnichenko, 2015
+ * @copyright Serge Rodovnichenko, 2015-2022
  * @license http://www.webasyst.com/terms/#eula Webasyst
  * @package asn
  */
@@ -15,14 +14,23 @@
  */
 class shopAsnPlugin extends shopPlugin
 {
-    public function hookProductSave($data)
+    /**
+     * @EventHandler product_save
+     * @param $data
+     * @return void
+     */
+    public function hookProductSave($data): void
     {
         if ($this->getSettings('use')) {
             $product = ifset($data['instance']);
             if ($product && isset($product['skus']) && is_array($product['skus'])) {
                 $Sku = new shopProductSkusModel();
                 $skus = array();
-                $view = wa()->getView();
+                try {
+                    $view = wa()->getView();
+                } catch (waException $e) {
+                    return;
+                }
                 $template = $this->getSettings('template');
                 if (empty($template)) {
                     $template = '{$sku_id}';
@@ -32,14 +40,18 @@ class shopAsnPlugin extends shopPlugin
                     $skus[$key] = $sku;
                     if (isset($sku['sku'], $sku['id']) && !strlen($sku['sku'])) {
                         $view->assign('sku_id', $sku['id']);
-                        $sku['sku'] = $view->fetch('string:' . $template);
-                        $new_sku = $Sku->update($sku['id'], $sku);
-                        if ($new_sku) {
-                            $skus[$key] = $sku;
-                        } else {
-                            if (wa()->getConfig()->isDebug()) {
-                                waLog::log("Error updating ID of SKU. Data: " . print_r($sku, true), 'asn.log');
+                        try {
+                            $sku['sku'] = $view->fetch('string:' . $template);
+                            $new_sku = $Sku->update($sku['id'], $sku);
+                            if ($new_sku) {
+                                $skus[$key] = $sku;
+                            } else {
+                                if (wa()->getConfig()::isDebug()) {
+                                    waLog::log("Error updating ID of SKU. Data: " . print_r($sku, true), 'asn.log');
+                                }
                             }
+                        } catch (waException|SmartyException $e) {
+                            //do nothing
                         }
                     }
                 }
